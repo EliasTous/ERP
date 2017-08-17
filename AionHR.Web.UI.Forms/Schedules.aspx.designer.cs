02d66ad58d0535f6a48541da8a9924daaa6981bf
eliastoss <elias.toss@softmachine.co>
2017-08-17 14:38:10 +0300

    Schedule Done

--- AionHR.Web.UI.Forms/Schedules.aspx.designer.cs
+++ AionHR.Web.UI.Forms/Schedules.aspx.designer.cs
@@ -463,6 +463,15 @@ namespace AionHR.Web.UI.Forms {
         protected global::Ext.Net.Button patternButton;
         
         /// <summary>
+        /// ScheduleNamelb control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Ext.Net.Label ScheduleNamelb;
+        
+        /// <summary>
         /// colDayName control.
         /// </summary>
         /// <remarks>
