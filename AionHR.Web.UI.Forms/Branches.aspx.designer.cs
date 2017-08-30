7b899b90d1bb04e16b55860f321b4bedf9076a90
eliastoss <elias.toss@softmachine.co>
2017-08-30 12:38:16 +0300

    Branch Report

--- AionHR.Web.UI.Forms/Branches.aspx.designer.cs
+++ AionHR.Web.UI.Forms/Branches.aspx.designer.cs
@@ -76,6 +76,24 @@ namespace AionHR.Web.UI.Forms {
         protected global::Ext.Net.Hidden timeZoneOffset;
         
         /// <summary>
+        /// Yes control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Ext.Net.Hidden Yes;
+        
+        /// <summary>
+        /// No control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Ext.Net.Hidden No;
+        
+        /// <summary>
         /// Store1 control.
         /// </summary>
         /// <remarks>
