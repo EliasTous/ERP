d7b9e5927893083e90fa420682f999e713b726b7
rabih attar <rabih.attar@softmachine.co>
2017-09-05 10:27:11 +0300

    local.RT204

---
+++ Reports/RightToWork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace Reports
+{
+    public partial class RightToWork : DevExpress.XtraReports.UI.XtraReport
+    {
+        public RightToWork()
+        {
+            InitializeComponent();
+        }
+
+        private void xrLabel3_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+        {
+
+        }
+
+        private void GroupHeader1_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+        {
+            e.Cancel = RowCount > 0;
+        }
+    }
+}
