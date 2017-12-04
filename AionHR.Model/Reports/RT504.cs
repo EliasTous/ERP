f63034b6fa145e2fd23b1e9523f107a30454b1cd
Elias Toss <elias.toss@softmachine.co>
2017-12-04 11:03:37 +0200

    04/12/2017

---
+++ AionHR.Model/Reports/RT504.cs
@@ -0,0 +1,29 @@
+﻿using AionHR.Model.Employees.Profile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AionHR.Model.Reports
+{
+  public  class RT504
+    {
+        public EmployeeName name { get; set; }
+        public int ssAmount { get; set; }
+        public int nssAmount { get; set; }
+        public int dAmount { get; set; }
+        public int netSalary { get; set; }
+
+        public string idRef { get; set; }
+        public string accountNumber { get; set; }
+        public string swiftCode { get; set; }
+       
+       
+           
+          
+          
+           
+            
+    }
+}
--- AionHR.Web.UI.Forms/PayrollGeneration.aspx.cs
+++ AionHR.Web.UI.Forms/PayrollGeneration.aspx.cs
@@ -1296,8 +1296,10 @@ namespace AionHR.Web.UI.Forms
         {
             
 
-            ListRequest departmentsRequest = new ListRequest();
-            ListResponse<Department> resp = _companyStructureService.ChildGetAll<Department>(departmentsRequest);
+            DepartmentListRequest req = new DepartmentListRequest();
+            req.type = 0; 
+            
+            ListResponse<Department> resp = _companyStructureService.ChildGetAll<Department>(req);
             if (!resp.Success)
                 X.Msg.Alert(Resources.Common.Error, GetGlobalResourceObject("Errors", resp.ErrorCode) != null ? GetGlobalResourceObject("Errors", resp.ErrorCode).ToString() + "<br>Technical Error: " + resp.ErrorCode + "<br> Summary: " + resp.Summary : resp.Summary).Show();
             departmentStore.DataSource = resp.Items;
