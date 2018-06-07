420d2e845f95d64bb678960b89f73b531792609d
elias.toss@softmachine.co <Elias@123>
2018-06-07 14:02:58 +0300

    07/06/2018

---
+++ AionHR.Model/TimeAttendance/GenerateAttendanceShift.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AionHR.Model.TimeAttendance
+{
+    public class GenerateAttendanceShift
+    {
+        public int branchId { get; set; }
+        public string fromDayId { get; set; }
+        public string toDayId { get; set; }
+    }
+}
