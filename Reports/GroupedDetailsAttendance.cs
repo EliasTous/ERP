d8200a722e4cb4e17c5ed931e1980b2e9e0009e7
elias.toss@softmachine.co <Elias@123>
2019-01-28 17:12:52 +0200

    28/01/2019 -3

---
+++ Reports/GroupedDetailsAttendance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace Reports
+{
+    public partial class GroupedDetailsAttendance : DevExpress.XtraReports.UI.XtraReport
+    {
+        public GroupedDetailsAttendance()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
