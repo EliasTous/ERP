5b784c556e545dcdca3808b03aa6f31736d36e24
elias.toss@softmachine.co <elias.toss@softmachine.co>
2019-07-02 09:15:32 +0300

    add last generated schedule to system defaults

--- AionHR.Web.UI.Forms/SystemDefaults.aspx.designer.cs
+++ AionHR.Web.UI.Forms/SystemDefaults.aspx.designer.cs
@@ -490,6 +490,15 @@ namespace AionHR.Web.UI.Forms {
         protected global::Ext.Net.TextField localServerIP;
         
         /// <summary>
+        /// lastGenFSDateTime control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Ext.Net.TextField lastGenFSDateTime;
+        
+        /// <summary>
         /// sourceTASC control.
         /// </summary>
         /// <remarks>
